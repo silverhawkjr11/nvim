2501e024ddbee3bd6ec835878fd1c3b7a85ca106
saeed <saeed.kanawat@gmail.com>
2024-12-19 18:39:14 +0200

    19-12-2024

--- init.lua
+++ init.lua
@@ -1,5 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- Enable line wrapping and breaking at word boundaries
+vim.o.wrap = true
+vim.o.linebreak = true
+
+-- Optional: Add a visible marker for wrapped lines
+vim.o.showbreak = "↪ " -- You can replace ↪ with any symbol or string
+
+-- Enable 24-bit colors and set colorscheme
 vim.o.termguicolors = true
 vim.cmd("colorscheme catppuccin")
