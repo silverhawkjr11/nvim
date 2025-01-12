4e6edef5e56a8c2ecc1b201232ac8a087723a382
silverhawkjr11 <saeed.kanawat@gmail.com>
2025-01-12 14:50:46 +0200

    dooing

--- lua/plugins/html-css.lua
+++ lua/plugins/html-css.lua
@@ -3,5 +3,7 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-lua/plenary.nvim",
+
+    "hrsh7th/nvim-cmp",
   },
 }
---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,9 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    -- Common dependencies for nvim-cmp
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp",
+  },
+}
